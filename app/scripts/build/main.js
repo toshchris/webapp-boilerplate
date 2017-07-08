f2fec9475dec68963b855127e43624d529959266
Christopher Goodwin <chris@toshchris.com>
2017-07-08 19:54:07 +0100

    First commit

---
+++ app/scripts/build/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+console.log('\'Allo \'Allo!');
+
+var one = true;
+var two = true;
+var three = true;
+var four = true;
+
+console.log('1+1 = ' + (1 + 1));
+
+//
+//   keys.forEach(key => key.addEventListener('transitionend', removeTransition))
---
+++ app/scripts/main.js
@@ -0,0 +1,12 @@
+console.log('\'Allo \'Allo!');
+
+const one = true;
+let two = true;
+let three = true;
+let four = true;
+
+console.log(`1+1 = ${ 1 + 1 }`);
+
+
+//
+//   keys.forEach(key => key.addEventListener('transitionend', removeTransition))
